91cd2b0d8be430ea297c5b70098696814ac8f379
Omkar Abhang <oabhang15@gmail.com>
2023-08-22 17:04:27 +0530

    Update utils.js
    Edit Functionalities

--- File Explorer Using Flux Arch/utils.js
+++ File Explorer Using Flux Arch/utils.js
@@ -1,5 +1,4 @@
 // Reference of Pure Function
-
 const createFolderItem = (action) => {
     let folderName = document.getElementById("naming");
     if (!folderName.value) {
@@ -8,7 +7,9 @@ const createFolderItem = (action) => {
     }
 
     let newLevel = findId(action);
-
+    if (newLevel == false) {
+        return;
+    }
     // Create a new div element for the folder item
     let childrenContainer = createChildrenContainer(folderName, "Folder");
     let newFolderDiv = createNewDiv(folderName, "Folder");
@@ -16,13 +17,15 @@ const createFolderItem = (action) => {
     // Create the child elements for the new folder item
     let arrowSpan = createArrow(folderName);
     let nameSpan = nameContainer(folderName);
+    let editSpan = createEditFolder(folderName);
     let folderSpan = createFolder(folderName);
     let fileSpan = createFile(folderName);
     let deleteSpan = createDeleteBtn(folderName);
+
     // Append the child elements to the new folder div
     newFolderDiv.appendChild(arrowSpan);
     newFolderDiv.appendChild(nameSpan);
-    // newFolderDiv.appendChild(editSpan);
+    newFolderDiv.appendChild(editSpan);
     newFolderDiv.appendChild(folderSpan);
     newFolderDiv.appendChild(fileSpan);
     newFolderDiv.appendChild(deleteSpan);
@@ -33,11 +36,9 @@ const createFolderItem = (action) => {
 
     // Append the new folder div to the file explorer
     // let fileExplorer = document.getElementById("fileExplorer");
-    let temp = document.getElementById(action.event.id).parentNode.parentNode
-        .id;
-    let targetDiv = temp + "Children";
-    let childDiv = document.getElementById(targetDiv);
-    childDiv.appendChild(newFolderDiv);
+    let targetDiv = document.getElementById(action.event.id).parentNode
+        .parentNode;
+    targetDiv.appendChild(newFolderDiv);
     folderName.style.displayText = "none";
 };
 
@@ -49,7 +50,9 @@ const createFileItem = (action) => {
     }
 
     let newLevel = findId(action);
-
+    if (newLevel == false) {
+        return;
+    }
     // Create a new div element for the folder item
     let newFileDiv = createNewDiv(fileName, "File");
     let childrenContainer = createChildrenContainer(fileName, "File");
@@ -59,7 +62,7 @@ const createFileItem = (action) => {
     let nameSpan = nameContainer(fileName);
     let fileSpan = createFile(fileName);
     let deleteSpan = createDeleteBtn(fileName);
-    // let editSpan = createEditFile(fileName);
+    let editSpan = createEditFile(fileName);
 
     // Append the child elements to the new folder div
     newFileDiv.appendChild(arrowSpan);
@@ -72,94 +75,143 @@ const createFileItem = (action) => {
     newLevel = newLevel * 10;
     newFileDiv.style.paddingLeft = newLevel + "px";
 
-    let temp = document.getElementById(action.event.id).parentNode.parentNode
-        .id;
-    let targetDiv = temp + "Children";
-    let childDiv = document.getElementById(targetDiv);
-    childDiv.appendChild(newFileDiv);
-    fileName.innerHTML = null;
-    // Append the new folder div to the file explorer
-    // let fileExplorer = document.getElementById("fileExplorer");
-    // fileExplorer.appendChild(newFileDiv);
+    let targetDiv = document.getElementById(action.event.id).parentNode
+        .parentNode;
+    targetDiv.appendChild(newFileDiv);
+    fileName.style.displayText = "none";
+
+    // // roating arrow
+    // let btn = document.getElementById(action.event.id);
+    // let btnSpan = btn.parentNode;
+    // let parent = btnSpan.parentNode;
+    // let arrow = parent.children[0];
 };
 
 const deleteFolderOrFile = (action) => {
     // Deleted
+    let parentId = document.getElementById(action.event.id).parentNode
+        .parentNode;
+    let nameOfItem = parentId.children[1].innerHTML;
+    updateSet(nameOfItem);
     let deleted = findId(action);
-    let grandParentId = document.getElementById(action.event.id).parentElement
-        .parentElement.parentNode.id;
+    let grandParentId = document.getElementById(action.event.id).parentNode
+        .parentNode.parentNode.id;
     let grandParent = document.getElementById(grandParentId);
-    grandParent.removeChild(grandParent.firstChild);
+    grandParent.removeChild(document.getElementById(parentId.id));
     return deleted;
 };
 
-// const editItem = (action) => {
-//     let { newName, prevName } = findId(action);
-//     editIds(action, newName, prevName);
-// };
+const editFolder = (action) => {
+    let { newName, prevName } = findId(action);
 
-// const editIds = (action, newName, prevName) => {
-//     console.log(action);
-//     console.log(newName);
-//     console.log(prevName);
-//     if (action.type === "FolderEdit") {
-//         let cleanNamePrev = prevName.replace(/ /g, "_");
-//         let cleanNameNew = newName.replace(/ /g, "_");
+    editFolderDiv(newName, prevName, "FolderDiv");
+    editArrow(newName, prevName, "ArrowSpan");
+    editArrowImg(newName, prevName, "ChildrenImg");
+    editNameSpan(newName, prevName);
+    editBtnImg(newName, prevName, "Edit");
+    editBtnImg(newName, prevName, "EditFolderBtn");
+    editFolderSpan(newName, prevName, "Folder");
+    editFolderImg(newName, prevName, "FolderBtn");
+    editDeleteSpan(newName, prevName, "Delete");
+    editDeleteImg(newName, prevName, "DeleteBtn");
+    editChildDiv(newName, prevName, "FolderDivChildren");
+};
 
-//         let idToEdit = cleanNamePrev + "FolderBtn";
-//         let folderBtn = document.getElementById(idToEdit);
-//         folderBtn.id = cleanNameNew + "FolderBtn";
-//         idToEdit = cleanNamePrev + "FolderSpan";
-//         let folderSpan = document.getElementById(idToEdit);
-//         folderSpan.id = cleanNameNew + "FolderSpan";
+const editFile = (action) => {
+    let { newName, prevName } = findId(action);
 
-//         idToEdit = cleanNamePrev + "FileBtn";
-//         let fileBtn = document.getElementById(idToEdit);
-//         fileBtn.id = cleanNameNew + "FileBtn";
-//         idToEdit = cleanNamePrev + "FileSpan";
-//         let fileSpan = document.getElementById(idToEdit);
-//         fileSpan.id = cleanNameNew + "FileSpan";
+    editFileDiv(newName, prevName, "FileDiv");
 
-//         idToEdit = cleanNamePrev + "DeleteBtn";
-//         let deleteBtn = document.getElementById(idToEdit);
-//         deleteBtn.id = cleanNameNew + "DeleteBtn";
-//         idToEdit = cleanNamePrev + "DeleteSpan";
-//         let deleteSpan = document.getElementById(idToEdit);
-//         deleteSpan.id = cleanNameNew + "DeleteSpan";
+    editArrow(newName, prevName, "ArrowSpan");
+    editArrowImg(newName, prevName, "ChildrenImg");
 
-//         idToEdit = cleanNamePrev + "NameSpan";
-//         let nameSpan = document.getElementById(idToEdit);
-//         let parent = document.getElementById(idToEdit).parentNode;
-//         console.log("parent", parent);
-//         nameSpan.id = cleanNameNew + "NameSpan";
-//         nameSpan.innerText = newName;
-//     } else {
-//         let cleanNamePrev = prevName.replace(/ /g, "_");
-//         let cleanNameNew = prevName.replace(/ /g, "_");
+    editNameSpan(newName, prevName);
 
-//         idToEdit = cleanNamePrev + "FileBtn";
-//         let fileBtn = document.getElementById(idToEdit);
-//         fileBtn.id = cleanNameNew + "FileBtn";
-//         idToEdit = cleanNamePrev + "FileSpan";
-//         let fileSpan = document.getElementById(idToEdit);
-//         fileSpan.id = cleanNameNew + "FileSpan";
+    editBtnSpan(newName, prevName, "Edit");
+    editBtnImg(newName, prevName, "EditFileBtn");
 
-//         idToEdit = cleanNamePrev + "deleteBtn";
-//         let deleteBtn = document.getElementById(idToEdit);
-//         deleteBtn.id = cleanNameNew + "DeleteBtn";
-//         idToEdit = cleanNamePrev + "DeleteSpan";
-//         let deleteSpan = document.getElementById(idToEdit);
-//         deleteSpan.id = cleanNameNew + "DeleteSpan";
+    editFileSpan(newName, prevName, "File");
+    editFileImg(newName, prevName, "FileBtn");
 
-//         idToEdit = cleanNamePrev + "NameSpan";
-//         let parent = document.getElementById(idToEdit).parentNode;
-//         console.log("parent", parent);
-//         // parent.id = cleanNameNew  + ""
-//         let nameSpan = document.getElementById(idToEdit);
-//         nameSpan.id = cleanNameNew + "NameSpan";
-//         nameSpan.innerText = newName;
-//     }
-// };
+    editDeleteSpan(newName, prevName, "Delete");
+    editDeleteImg(newName, prevName, "DeleteBtn");
+
+    editChildDiv(newName, prevName, "FileDivChildren");
+};
+
+const editFolderDiv = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+
+const editFileDiv = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+
+const editArrow = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editArrowImg = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editNameSpan = (newName, prevName) => {
+    let id = prevName.replace(/ /g, "_");
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_");
+    ele.innerText = newName;
+};
+const editBtnSpan = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editBtnImg = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editFolderSpan = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editFolderImg = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editFileSpan = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editFileImg = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editDeleteSpan = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editDeleteImg = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
+const editChildDiv = (newName, prevName, suffix) => {
+    let id = prevName.replace(/ /g, "_") + suffix;
+    let ele = document.getElementById(id);
+    ele.id = newName.replace(/ /g, "_") + suffix;
+};
 
 const createNewDiv = (folderName, type) => {
     if (type === "Folder") {
@@ -180,6 +232,7 @@ const toggleBtn = (action) => {
     let arrowSpan = arrowImg.parentNode;
     let parent = arrowSpan.parentNode;
     let children = parent.children[5];
+    let arrow = parent.children[0];
     if (children.style.display === "none") {
         children.style.display = "block";
     } else {
@@ -200,26 +253,24 @@ const createChildrenContainer = (name, type) => {
 const createEditFolder = (userInput) => {
     let editSpan = document.createElement("span");
     let editBtn = document.createElement("img");
-    // folderBtn.addEventListener("click", findId);
     editBtn.src = "images/icons8-edit-16.png";
     editBtn.className = "editBtn";
     editBtn.id = userInput.value.replace(/ /g, "_") + "EditFolderBtn";
     editBtn.setAttribute("onclick", "onClickEdit(this)");
     editSpan.appendChild(editBtn);
-    editSpan.id = userInput.value.replace(/ /g, "_") + "EditSpan";
+    editSpan.id = userInput.value.replace(/ /g, "_") + "Edit";
     return editSpan;
 };
 
 const createEditFile = (userInput) => {
     let editSpan = document.createElement("span");
     let editBtn = document.createElement("img");
-    // folderBtn.addEventListener("click", findId);
     editBtn.src = "images/icons8-edit-16.png";
     editBtn.className = "editBtn";
     editBtn.id = userInput.value.replace(/ /g, "_") + "EditFileBtn";
     editBtn.setAttribute("onclick", "onClickEdit(this)");
     editSpan.appendChild(editBtn);
-    editSpan.id = userInput.value.replace(/ /g, "_") + "EditSpan";
+    editSpan.id = userInput.value.replace(/ /g, "_") + "Edit";
     return editSpan;
 };
 
@@ -233,7 +284,7 @@ const createFolder = (folderName) => {
     folderBtn.id = folderName.value.replace(/ /g, "_") + "FolderBtn";
     folderBtn.setAttribute("onclick", "onClickFolder(this)");
     folderSpan.appendChild(folderBtn);
-    folderSpan.id = folderName.value.replace(/ /g, "_") + "FolderSpan";
+    folderSpan.id = folderName.value.replace(/ /g, "_") + "Folder";
     return folderSpan;
 };
 
@@ -246,7 +297,7 @@ const createFile = (fileName) => {
     fileBtn.id = fileName.value.replace(/ /g, "_") + "FileBtn";
     fileBtn.setAttribute("onclick", "onClickFile(this)");
     fileSpan.appendChild(fileBtn);
-    fileSpan.id = fileName.value.replace(/ /g, "_") + "FileSpan";
+    fileSpan.id = fileName.value.replace(/ /g, "_") + "File";
     return fileSpan;
 };
 
@@ -260,7 +311,7 @@ const createDeleteBtn = (userInput) => {
     deleteBtn.setAttribute("onclick", "onClickDelete(this)");
     deleteBtn.textContent = "🗑";
     deleteSpan.appendChild(deleteBtn);
-    deleteSpan.id = userInput.value.replace(/ /g, "_") + "DeleteSpan";
+    deleteSpan.id = userInput.value.replace(/ /g, "_") + "Delete";
     return deleteSpan;
 };
 
@@ -283,7 +334,7 @@ const nameContainer = (folderName) => {
     const userInput = document.getElementById("naming");
     nameSpan.textContent = `${userInput.value}`;
     nameSpan.style.fontSize = "30px";
-    nameSpan.id = folderName.value.replace(/ /g, "_") + "NameSpan";
+    nameSpan.id = folderName.value.replace(/ /g, "_");
     return nameSpan;
 };
 
@@ -299,35 +350,3 @@ const toggleDarkMode = () => {
         toogleBtn.innerHTML = "Dark Mode";
     }
 };
-
-// const createFileItem = (event) => {
-//     let fileName = document.getElementById("naming");
-
-//     // Create a new div element for the folder item
-//     let newFileDiv = document.createElement("div");
-//     newFileDiv.className = "item"; // Set the CSS class
-//     newFileDiv.id = fileName.value.replace(/ /g, "_") + "FileDiv";
-
-//     // Create the child elements for the new folder item
-//     let arrowSpan = createArrow();
-//     arrowSpan.id = fileName + "ArrowSpan";
-
-//     let nameSpan = nameContainer(fileName);
-//     nameSpan.id = fileName.value.replace(/ /g, "_") + "NameSpan";
-
-//     let fileSpan = createFile(fileName);
-//     fileSpan.id = fileName.value.replace(/ /g, "_") + "FileSpan";
-
-//     let deleteSpan = createDeleteBtn(fileName);
-//     deleteSpan.id = fileName.value.replace(/ /g, "_") + "DeleteSpan";
-
-//     // Append the child elements to the new folder div
-//     newFileDiv.appendChild(arrowSpan);
-//     newFileDiv.appendChild(nameSpan);
-//     newFileDiv.appendChild(fileSpan);
-//     newFileDiv.appendChild(deleteSpan);
-
-//     // Append the new folder div to the file explorer
-//     let fileExplorer = document.getElementById("fileExplorer");
-//     fileExplorer.appendChild(newFileDiv);
-// };
